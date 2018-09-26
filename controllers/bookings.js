a3321b3f12ae569f8805241ca3c8b168d6182a92
Ahmed <ahmed.hamdy.awwad@gmail.com>
2018-09-26 06:37:45 +0200

    Adding event handler for the services selector.

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -7,7 +7,8 @@ var con = db.createConnection({
 	user: "root",
 	password: "123456",
 	database: "clinics",
-	port: 3306
+	port: 3306,
+	multipleStatements: true
   });
 
   con.connect(function(err) {
@@ -17,10 +18,13 @@ var con = db.createConnection({
 
 module.exports = function(server){
 server.get('/bookings', function(req, res){
-	query1 = "SELECT bookings.id, bookings.bookingDate, patients.patientName, services.name as serviceName, employees.empName, services.cost, notes, discount, installments FROM bookings join patients on bookings.patientName = patients.id join services on bookings.service = services.id join employees on bookings.doctor = employees.id;"
-	query2 = "select * from patients;"
-	con.query(query1, function (err, data) {
+	query1 = "SELECT bookings.id, bookings.bookingDate, patients.patientName, services.name as serviceName, employees.empName, services.cost, notes, discount, installments FROM bookings join patients on bookings.patientName = patients.id join services on bookings.service = services.id join employees on bookings.doctor = employees.id; select id, patientName from patients order by patientName; select * from services; select id, empName from employees where category = 'Medical' order by empName;"
+	con.query(query1, [4, 1], function (err, data) {
 		if (err) throw err;
+		// console.log(data[0]);
+		// console.log(data[1]);
+		// console.log(data[2]);
+		// console.log(data[3]);
 		res.render('bookings', {data: data});
 		console.log('bookings\' data retrieved.')
 		});
@@ -28,14 +32,15 @@ server.get('/bookings', function(req, res){
 
 server.post('/bookings', urlencoderParser, function(req, res){
 	data = req.body;
-	con.query('INSERT INTO bookings SET ?', data, function (error) {
-		if (error) throw error;
-		console.log('booking\'s data created');
-		});
-		con.query("select * from bookings", function (err, data) {
-		if (err) throw err;
-		res.render('bookings', {data: data});
-		console.log('bookings\' data retrieved.')
-		});
+	console.log(data);
+	// con.query('INSERT INTO bookings SET ?', data, function (error) {
+	// 	if (error) throw error;
+	// 	console.log('booking\'s data created');
+	// 	});
+	// 	con.query("select * from bookings", function (err, data) {
+	// 	if (err) throw err;
+	// 	res.render('bookings', {data: data});
+	// 	console.log('bookings\' data retrieved.')
+	// 	});
 });
 };
