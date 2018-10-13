f3c40179cb33c8304c659216b309436b61081f31
Ahmed <ahmed.hamdy.awwad@gmail.com>
2018-10-13 08:07:32 +0200

    Fixed bugs in the invoice where invoice's data did not appear correctly

--- controllers/createTrx.js
+++ controllers/createTrx.js
@@ -17,12 +17,17 @@ var con = db.createConnection({
 
 module.exports = function(server){
 
-	server.get('/createTrx', function(req, res){
+	server.get('/createTrx', urlencoderParser, function(req, res){
 		data = req.query;
 		con.query('INSERT INTO transactions SET ?', data, function (error) {
 			if (error) throw error;
 			console.log('Visit\'s invoice created');
 			});
-		res.redirect('home');
+		con.query("select * from (select bookings.id as bookingID, patients.patientName, patients.age, services.name as service, employees.empName as doctor from bookings join patients on bookings.patientName = patients.id join services on bookings.service = services.id join employees on bookings.doctor = employees.id) as a join (select bookingNum, payment, visits.id as visitID, transactions.id, transactions.trxDate from visits join transactions on visits.id = transactions.visit) as b on a.bookingID = b.bookingNum where b.visitID = ?", data.visit, function (err, data) {
+			if (err) throw err;
+			console.log(data)
+			res.render('invoice', {data: data});
+			});
+		// res.render('invoice')
 		});
 };
